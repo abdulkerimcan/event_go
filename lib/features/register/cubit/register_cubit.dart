23f72af749afcbaad546d07da43a36cd0d05f1c8
abdulkerimcan <abdulkerimcann@outlook.com>
2023-08-23 16:05:51 +0300

    Integrated Firebase authentication to register

--- lib/features/register/cubit/register_cubit.dart
+++ lib/features/register/cubit/register_cubit.dart
@@ -1,14 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eventgo/features/register/cubit/register_state.dart';
+import 'package:eventgo/product/model/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
-  
   final GlobalKey<FormState> formKey;
   bool _isLoading = false;
   bool _isFailed = false;
-  RegisterCubit(super.initialState,
-      this.formKey);
+  final auth = FirebaseAuth.instance;
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  RegisterCubit(super.initialState, this.formKey);
 
   Future<void> _changeLoading() async {
     _isLoading = !_isLoading;
@@ -16,18 +21,37 @@ class RegisterCubit extends Cubit<RegisterState> {
     emit(RegisterLoadingState(_isLoading));
   }
 
-  Future<void> register(String name, String surname, String email, String password) async {
+  Future<void> register(
+      String name, String surname, String email, String password) async {
     if (formKey.currentState != null && formKey.currentState!.validate()) {
       _isFailed = false;
+      try {
+        _changeLoading();
+        var userResult = await auth.createUserWithEmailAndPassword(
+            email: email, password: password);
 
+        if (userResult.user != null) {
+          CollectionReference users =
+              FirebaseFirestore.instance.collection('Users');
+          var user = UserModel(name: name, surname: surname, email: email);
+          final docRef = users
+              .withConverter(
+                  fromFirestore: UserModel.fromFirestore,
+                  toFirestore: (UserModel usermodel, options) =>
+                      user.toFirestore())
+              .doc(userResult.user?.uid);
+          await docRef
+              .set(user)
+              .whenComplete(() => emit(const RegisterCompleteState()));
+        }
+      } on FirebaseAuthException catch (e) {
+        _isFailed = true;
+        emit(RegisterValidateState(isValidate: _isFailed, message: e.message));
+      }
       _changeLoading();
-      await Future.delayed(const Duration(seconds: 3));
-      _changeLoading();
-      emit(const RegisterCompleteState());
-
     } else {
       _isFailed = true;
-      emit(RegisterValidateState(_isFailed));
+      emit(RegisterValidateState(isValidate: _isFailed));
     }
   }
 }
